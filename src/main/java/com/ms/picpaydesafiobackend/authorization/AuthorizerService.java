fbb07dfdbb9e08823b3758f09b6d3c9ec2860f7d
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-25 15:16:30 -0300

    adicionado log de informação

--- src/main/java/com/ms/picpaydesafiobackend/authorization/AuthorizerService.java
+++ src/main/java/com/ms/picpaydesafiobackend/authorization/AuthorizerService.java
@@ -1,11 +1,16 @@
 package com.ms.picpaydesafiobackend.authorization;
 
+import com.ms.picpaydesafiobackend.notification.NotificationService;
 import com.ms.picpaydesafiobackend.transaction.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class AuthorizerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
 
     private RestClient restClient;
 
@@ -15,11 +20,13 @@ public class AuthorizerService {
                 .build();
     }
     public void authorize(Transaction transaction){
+        LOGGER.info("Authorizing transaction...{}", transaction);
         var response = restClient.get()
                 .retrieve()
                 .toEntity(Authorization.class);
         if (response.getStatusCode().isError() || !response.getBody().isAuthorized()){
             throw new UnauthorizedTransactionException("Unauthorized transaction!");
         }
+        LOGGER.info("Authorized transaction! {}", transaction);
     }
 }
