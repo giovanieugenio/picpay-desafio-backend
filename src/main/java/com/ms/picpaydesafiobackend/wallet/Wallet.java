42fbd6f54fbf8ab98ae8836273d0f6efbabedc28
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-25 15:20:24 -0300

    adicionado anotação Table na entidade wWllet

--- src/main/java/com/ms/picpaydesafiobackend/wallet/Wallet.java
+++ src/main/java/com/ms/picpaydesafiobackend/wallet/Wallet.java
@@ -1,9 +1,11 @@
 package com.ms.picpaydesafiobackend.wallet;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 
+@Table(name = "WALLETS")
 public record Wallet(
         @Id Long id,
         String fullName,
