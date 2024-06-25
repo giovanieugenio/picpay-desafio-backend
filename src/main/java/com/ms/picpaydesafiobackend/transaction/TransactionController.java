6fce44098ed997c3f341ad91458593cdf49e8cab
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-25 14:28:20 -0300

    criado a classe de Controller

--- src/main/java/com/ms/picpaydesafiobackend/transaction/TransactionController.java
+++ src/main/java/com/ms/picpaydesafiobackend/transaction/TransactionController.java
@@ -1,4 +1,30 @@
 package com.ms.picpaydesafiobackend.transaction;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService){
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public Transaction createTransation(@RequestBody Transaction transaction){
+        return transactionService.create(transaction);
+    }
+
+    @GetMapping
+    public List<Transaction> list(){
+        return transactionService.list();
+    }
 }
