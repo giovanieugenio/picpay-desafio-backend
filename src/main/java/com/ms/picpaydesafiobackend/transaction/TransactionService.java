4edb31f3cc8cafdebd5a8fc80b6bb9652cc75b71
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-25 14:29:42 -0300

    adicionado metodo de lista para transação

--- src/main/java/com/ms/picpaydesafiobackend/transaction/TransactionService.java
+++ src/main/java/com/ms/picpaydesafiobackend/transaction/TransactionService.java
@@ -9,6 +9,8 @@ import com.ms.picpaydesafiobackend.wallet.WalletType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class TransactionService {
 
@@ -55,5 +57,9 @@ public class TransactionService {
         return payer.type() == WalletType.COMMON.getValue() &&
                 payer.balance().compareTo(transaction.value()) >= 0 &&
                 !payer.id().equals(transaction.payee());
+    }
+
+    public List<Transaction> list() {
+        return transactionRepository.findAll();
     }
 }
