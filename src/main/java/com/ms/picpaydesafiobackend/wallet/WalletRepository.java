1227808b49f8abd1e2b74f06ad582e99119a636f
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-24 21:15:01 -0300

    criado o WalletRepository

---
+++ src/main/java/com/ms/picpaydesafiobackend/wallet/WalletRepository.java
@@ -0,0 +1,8 @@
+package com.ms.picpaydesafiobackend.wallet;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends CrudRepository<Wallet, Long> {
+}
